74a76ce614b751e400672bf08c92ff9f4fd3c70b
unknown <denis.hadzipasic@partyrent.com>
2024-03-13 08:49:14 +0100

    added role to the use model

--- models/user-model.js
+++ models/user-model.js
@@ -1,20 +1,27 @@
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-    logInID: {type: Number, required: true, unique: true},
-    password: {type: String, required: true}
-})
+  logInID: { type: Number, required: true, unique: true },
+  password: { type: String, required: true, select: false }, // why is password still sent in the response
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+});
 
 userSchema.pre("save", async function (next) {
-    if(this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 10)
-    }
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
 
-    next();
-})
+  next();
+});
 
-const User = model("User", userSchema)
+// userSchema.methods.toJSON = function() {
+//     const userObject = this.toObject();
+//     delete userObject.password;
+//     return userObject;
+// };
+
+const User = model("User", userSchema);
 
 module.exports = User;
