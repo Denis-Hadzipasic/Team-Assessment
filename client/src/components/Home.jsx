7418f2465f1f31f492c294147a0b1346a653d208
unknown <denis.hadzipasic@partyrent.com>
2024-03-15 16:50:07 +0100

    added another user to display the green progress bar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -48,7 +48,7 @@ export default function Home() {
             </div>
           </div>
           <div className="bg-white lg:w-3/12 md:7/12 w-8/12 shadow-3xl rounded-xl ml-auto mr-auto">
-            <form onSubmit={handleSubmit(onSubmit)} className="mt-14">
+            <form onSubmit={handleSubmit(onSubmit)} className="mt-20">
               <div className="flex items-center text-lg mb-6 md:mb-8 ">
                 <svg className="absolute ml-3" width="24" viewBox="0 0 24 24">
                   <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" />
@@ -131,7 +131,7 @@ export default function Home() {
                     </div>
 
                     <p className="mr-3 mt-3 font-semibold">
-                      Anmelde-ID: {user.logInID}
+                      Anmelde-ID: <span className="font-bold">{user.logInID}</span>
                     </p>
                   </div>
 
